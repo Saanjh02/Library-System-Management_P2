1830236f3fea72b362d0816338a20925ab82c66c
Sunita Jangid <sunitajangid0303@gmail.com>
2025-06-20 15:16:59 -0400

    Update Schemas.sql

--- Schemas.sql
+++ Schemas.sql
@@ -4,7 +4,7 @@
 DROP TABLE IF EXISTS branch
 CREATE TABLE branch
 				(
-                    branch_id VARCHAR(10) PRIMARY KEY,
+                                 	branch_id VARCHAR(10) PRIMARY KEY,
 					manager_id VARCHAR(10),
 					branch_address VARCHAR(55),
 					contact_no VARCHAR(15) 
